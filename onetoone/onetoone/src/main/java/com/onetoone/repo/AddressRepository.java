1b8d8aff3e26c35440f686191eee990bf73177bc
Karanam Bhimsen rao <brkaran2k2@gmail.com>
2025-02-13 13:39:34 +0530

    Delete onetoone directory

--- onetoone/onetoone/src/main/java/com/onetoone/repo/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.onetoone.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.onetoone.entity.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Integer> {
-java.util.List<Address> findByCity(String city);
-}
